c2095837b84706e384fa1ed2e9aa768d6e82d6d0
Tri <huutri231103@gmail.com>
2025-01-07 14:44:45 +0700

    Update finetune_wavlm_libri960_ft_char_hotwords.sh

--- examples/contextual_asr/scripts/finetune/finetune_wavlm_libri960_ft_char_hotwords.sh
+++ examples/contextual_asr/scripts/finetune/finetune_wavlm_libri960_ft_char_hotwords.sh
@@ -18,53 +18,53 @@ val_data_path=/kaggle/input/librispeech/librispeech_dev_other.jsonl
 
 output_dir=/kaggle/working/output/vicuna-7b-v1.5-WavLM-Large-libri960-ft-char-hotwords-$(date +"%Y%m%d")
 
-# # Thiết lập Hydra arguments
-# hydra_args="
-# hydra.run.dir=$output_dir \
-# ++model_config.llm_name=vicuna-7b-v1.5 \
-# ++model_config.llm_path=$llm_path \
-# ++model_config.llm_dim=4096 \
-# ++model_config.encoder_name=wavlm \
-# ++model_config.normalize=true \
-# ++dataset_config.normalize=true \
-# ++model_config.encoder_projector_ds_rate=5 \
-# ++model_config.encoder_path=$speech_encoder_path \
-# ++model_config.encoder_dim=1024 \
-# ++model_config.encoder_projector=cov1d-linear \
-# ++dataset_config.dataset=speech_dataset \
-# ++dataset_config.train_data_path=$train_data_path \
-# ++dataset_config.val_data_path=$val_data_path \
-# ++dataset_config.input_type=raw \
-# ++dataset_config.dataset=hotwords_dataset \
-# ++dataset_config.file=examples/contextual_asr/dataset/hotwords_dataset.py:get_speech_dataset \
-# ++train_config.model_name=asr \
-# ++train_config.num_epochs=5 \
-# ++train_config.freeze_encoder=true \
-# ++train_config.freeze_llm=true \
-# ++train_config.batching_strategy=custom \
-# ++train_config.warmup_steps=1000 \
-# ++train_config.total_steps=100000 \
-# ++train_config.lr=1e-4 \
-# ++train_config.validation_interval=8000 \
-# ++train_config.val_batch_size=4 \
-# ++train_config.batch_size_training=4 \
-# ++train_config.num_workers_dataloader=2 \
-# ++train_config.output_dir=$output_dir \
-# ++metric=acc \
-# ++log_config.log_file=$output_dir/train.log \
-# ++log_config.use_wandb=false \  # Tắt wandb nếu không cần
-# ++log_config.log_interval=5 \
-# "
+# Thiết lập Hydra arguments
+hydra_args="
+hydra.run.dir=$output_dir \
+++model_config.llm_name=vicuna-7b-v1.5 \
+++model_config.llm_path=$llm_path \
+++model_config.llm_dim=4096 \
+++model_config.encoder_name=wavlm \
+++model_config.normalize=true \
+++dataset_config.normalize=true \
+++model_config.encoder_projector_ds_rate=5 \
+++model_config.encoder_path=$speech_encoder_path \
+++model_config.encoder_dim=1024 \
+++model_config.encoder_projector=cov1d-linear \
+++dataset_config.dataset=speech_dataset \
+++dataset_config.train_data_path=$train_data_path \
+++dataset_config.val_data_path=$val_data_path \
+++dataset_config.input_type=raw \
+++dataset_config.dataset=hotwords_dataset \
+++dataset_config.file=examples/contextual_asr/dataset/hotwords_dataset.py:get_speech_dataset \
+++train_config.model_name=asr \
+++train_config.num_epochs=5 \
+++train_config.freeze_encoder=true \
+++train_config.freeze_llm=true \
+++train_config.batching_strategy=custom \
+++train_config.warmup_steps=1000 \
+++train_config.total_steps=100000 \
+++train_config.lr=1e-4 \
+++train_config.validation_interval=8000 \
+++train_config.val_batch_size=4 \
+++train_config.batch_size_training=4 \
+++train_config.num_workers_dataloader=2 \
+++train_config.output_dir=$output_dir \
+++metric=acc \
+++log_config.log_file=$output_dir/train.log \
+++log_config.use_wandb=false \  # Tắt wandb nếu không cần
+++log_config.log_interval=5 \
+"
 
-# # Huấn luyện với 2 GPU
-# torchrun \
-#     --nnodes=1 \  # Số node là 1 vì chạy trên một máy duy nhất
-#     --nproc_per_node=2 \  # Số GPU sẽ sử dụng
-#     --master_port=29504 \  # Cổng giao tiếp
-#     $code_dir/finetune_contextual_asr.py \
-#     --config-path "conf" \
-#     --config-name "prompt.yaml" \
-#     ++train_config.enable_fsdp=false \
-#     ++train_config.enable_ddp=true \
-#     ++train_config.use_fp16=true \
-#     $hydra_args
+# Huấn luyện với 2 GPU
+torchrun \
+    --nnodes=1 \  # Số node là 1 vì chạy trên một máy duy nhất
+    --nproc-per-node=2 \  # Số GPU sẽ sử dụng
+    --master_port=29504 \  # Cổng giao tiếp
+    $code_dir/finetune_contextual_asr.py \
+    --config-path "conf" \
+    --config-name "prompt.yaml" \
+    ++train_config.enable_fsdp=false \
+    ++train_config.enable_ddp=true \
+    ++train_config.use_fp16=true \
+    $hydra_args
